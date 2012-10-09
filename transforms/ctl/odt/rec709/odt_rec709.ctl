ampas/aces-core

commit 48d4d9dc841c42fe1f4d0bcaf1c6afcc1d5b88ab

replaces declared constant with function to calculate white point handling transform in ODTs
* adds xyY_to_XYZt to utilities
* adds calculate_cat_matrix to utilities

transforms/ctl/odt/rec709/odt_rec709.ctl
@@ -88,11 +88,9 @@ import "splines";
 
 
 /* ============ CONSTANTS ============ */
-const float CAT[3][3] = {
-	{0.98823, -0.00569, 0.00035},
-	{-0.00789, 0.99869, 0.00112},
-	{0.01676, 0.00667, 1.07808}
-};
+const float SRC_WHITE[3] = {ACESChromaticities.white[0], ACESChromaticities.white[1], 1.0};
+const float DEST_WHITE[3] = {rec709Chromaticities.white[0], rec709Chromaticities.white[1], 1.0};
+const float CAT[3][3] = calculate_cat_matrix( SRC_WHITE, DES_WHITE );
 
 
 
@@ -123,7 +121,7 @@ void main
 		float ACEStoXYZ[4][4] = RGBtoXYZ( ACESChromaticities, 1.0);
 		float XYZ[3] = mult_f3_f44( rgbPostTonecurve, ACEStoXYZ);
 
-		// Chromatic adaptation to D65
+		// Chromatic adaptation from ACES White to Assumed Observer Adapted White
 		XYZ = mult_f3_f33( XYZ, CAT);
 
 		// XYZ to Rec709

transforms/ctl/odt/sRGB/odt_sRGB.ctl
@@ -78,12 +78,9 @@ import "splines";
 
 
 /* ============ CONSTANTS ============ */
-const float CAT[3][3] = {
-	{0.98823, -0.00569, 0.00035},
-	{-0.00789, 0.99869, 0.00112},
-	{0.01676, 0.00667, 1.07808}
-};
-
+const float SRC_WHITE[3] = {ACESChromaticities.white[0], ACESChromaticities.white[1], 1.0};
+const float DEST_WHITE[3] = {rec709Chromaticities.white[0], rec709Chromaticities.white[1], 1.0};
+const float CAT[3][3] = calculate_cat_matrix( SRC_WHITE, DES_WHITE );
 
 
 
@@ -112,8 +109,8 @@ void main
 		float ACEStoXYZ[4][4] =  RGBtoXYZ( ACESChromaticities, 1.0);
 		float XYZ[3] = mult_f3_f44( rgbPostTonecurve, ACEStoXYZ);
 
-     	// Convert to D65
-     	XYZ = mult_f3_f33( XYZ, CAT);
+		// Chromatic adaptation from ACES White to Assumed Observer Adapted White
+		XYZ = mult_f3_f33( XYZ, CAT);
 	   
         // XYZ to Rec709
         float XYZtoRec709[4][4] =  XYZtoRGB( rec709Chromaticities, 1.0);

transforms/ctl/utilities/utilities.ctl
@@ -344,3 +344,67 @@ float[3] fullRange_to_smpteRange( varying float rgbIn[3] )
 	return rgbOut;
 }
 
+float[3] xyY_to_XYZt( float xyY[3] )
+{
+
+	//
+	// Converts CIE XYZ to CIE x, y, and Y
+	//
+
+	float z = 1-xyY[0]-xyY[1];
+	
+	float XYZt[3];
+	XYZt[0] = xyY[0] / xyY[1] * xyY[2];
+	XYZt[1] = xyY[2];
+	XYZt[2] = z / xyY[1] * xyY[2];
+	
+	return XYZt;
+	
+}
+
+
+const float CONERESPMATCAT02[3][3] = {
+{0.73280, -0.70360, 0.00300},
+{0.42960,  1.69750, 0.01360},
+{-0.16240, 0.00610, 0.98340}
+};
+
+const float CONERESPMATBRADFORD[3][3] = {
+{0.40024, -0.22630, 0.00000},
+{0.70760, 1.16532, 0.00000},
+{-0.08081, 0.04570, 0.91822}
+};
+
+
+float[3][3] calculate_cat_matrix( float src_xyY[3], 
+								  float des_xyY[3], 
+								  float coneRespMat[3][3] = CONERESPMATCAT02)
+								  
+{
+
+	//
+	// Calculates and returns a 3x3 Von Kries Chromatic Adaptation Transform to transform
+	// from src_xyY to des_xyY using the cone response primaries defined by coneRespMat.  
+	// By default coneRespMat is set to CONERESPMATCAT02 which are the cone primaries 
+	// defined in CIECAM02. The default coneRespMat can be overridden at runtime.  
+	// In most circumstances src_xyY[2] and des_xyY[2] should be equal to 1.0.
+	//
+
+	float src_XYZt[3] = xyY_to_XYZt( src_xyY );
+	float des_XYZt[3] = xyY_to_XYZt( des_xyY );
+	
+	float src_coneResp[3] = mult_f3_f33( src_XYZt, coneRespMat);
+	float des_coneResp[3] = mult_f3_f33( des_XYZt, coneRespMat);
+
+	float vkMat[3][3] = {
+	{ des_coneResp[0] / src_coneResp[0], 0.0, 0.0 },
+	{ 0.0, des_coneResp[1] / src_coneResp[1], 0.0 },
+	{ 0.0, 0.0, des_coneResp[2] / src_coneResp[2] }
+	};
+		
+	float cat_matrix[3][3] = mult_f33_f33( coneRespMat, transpose_f33( mult_f33_f33( transpose_f33( invert_f33( coneRespMat ) ), vkMat ) ) );
+	
+	return cat_matrix;
+	
+}
+

commit cc7d7a812047c07941fa2b427bedd03949056f01

fixes typos that were introduced in commit 48d4d9d

transforms/ctl/odt/rec709/odt_rec709.ctl
@@ -90,7 +90,7 @@ import "splines";
 /* ============ CONSTANTS ============ */
 const float SRC_WHITE[3] = {ACESChromaticities.white[0], ACESChromaticities.white[1], 1.0};
 const float DEST_WHITE[3] = {rec709Chromaticities.white[0], rec709Chromaticities.white[1], 1.0};
-const float CAT[3][3] = calculate_cat_matrix( SRC_WHITE, DES_WHITE );
+const float CAT[3][3] = calculate_cat_matrix( SRC_WHITE, DEST_WHITE );
 
 
 

transforms/ctl/odt/sRGB/odt_sRGB.ctl
@@ -80,7 +80,7 @@ import "splines";
 /* ============ CONSTANTS ============ */
 const float SRC_WHITE[3] = {ACESChromaticities.white[0], ACESChromaticities.white[1], 1.0};
 const float DEST_WHITE[3] = {rec709Chromaticities.white[0], rec709Chromaticities.white[1], 1.0};
-const float CAT[3][3] = calculate_cat_matrix( SRC_WHITE, DES_WHITE );
+const float CAT[3][3] = calculate_cat_matrix( SRC_WHITE, DEST_WHITE );
 
 
 

transforms/ctl/utilities/utilities.ctl
@@ -348,7 +348,7 @@ float[3] xyY_to_XYZt( float xyY[3] )
 {
 
 	//
-	// Converts CIE XYZ to CIE x, y, and Y
+	// Converts CIE x, y, and Y to CIE XYZ tristimulus values
 	//
 
 	float z = 1-xyY[0]-xyY[1];